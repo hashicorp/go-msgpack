6335e8d59d2b7c56167777c4b8789cf2acd84704
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2019-01-18 11:58:33 -0500

    Fix import complaining due to no package statement

--- codec/ext_dep_test.go
+++ codec/ext_dep_test.go
@@ -5,11 +5,12 @@
 // ugly deps. This can be run manually if further changes are ever made to this
 // lib.
 
-/*
 // Copyright (c) 2012, 2013 Ugorji Nwoke. All rights reserved.
 // Use of this source code is governed by a BSD-style license found in the LICENSE file.
 
 package codec
+
+/*
 
 // This file includes benchmarks which have dependencies on 3rdparty
 // packages (bson and vmihailenco/msgpack) which must be installed locally.

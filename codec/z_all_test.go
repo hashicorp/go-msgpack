634e932322694bbdad787dd94da4c973564ebeaf
Christopher Swenson <christopher.swenson@hashicorp.com>
2022-12-02 10:55:45 -0800

    Run gofmt

--- codec/z_all_test.go
+++ codec/z_all_test.go
@@ -1,8 +1,8 @@
 // Copyright (c) 2012-2018 Ugorji Nwoke. All rights reserved.
 // Use of this source code is governed by a MIT license found in the LICENSE file.
 
-// +build alltests
-// +build go1.7
+//go:build alltests && go1.7
+// +build alltests,go1.7
 
 package codec
 
